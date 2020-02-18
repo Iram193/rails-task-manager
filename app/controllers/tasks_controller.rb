97135761a09ed9ce793aead6f89513a4e29c3a08
Iram Shehzadi <Shehzadi193@gmail.com>
2020-02-18 14:58:15 +0000

    form complete

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+class TasksController < ApplicationController
+  before_action :find_task, only: [:show]
+
+  def index
+    @tasks = Task.all
+  end
+
+  def show; end
+
+  def new # display form
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to task_path(@task)
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+
+  def find_task
+    @task = Task.find(params[:id])
+ end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # add a new route to list the tasks
+  get '/tasks', to: 'tasks#index'
+  get '/tasks/:id', to: 'tasks#show', as: :task
+  get '/tasks/new', to: 'tasks#new' # display form
+  get '/tasks/new', to: 'tasks#show'
 end
